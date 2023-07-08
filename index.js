25812b9a38ddd177107d65f80bfff403162fa99d
Kevin Choi <kevinnwjchoi@gmail.com>
2023-07-07 22:43:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    kevin: {
+        street: "Leighlex",
+    },
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = { ...obj};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...obj};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
